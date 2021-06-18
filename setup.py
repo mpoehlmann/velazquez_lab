7811b855314c5be78b4e06edc769ab01b25ccf97
Michael Poehlmann <poehlmann@ucdavis.edu>
2021-06-18 14:03:08 -0700

    Testing.

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ import numpy as np
 with open('README.md') as f:
   readme = f.read()
 
+with open('requirements.txt') as f:
+  required = f.read().splitlines()
+
 setup(
   name='velazquez_lab',
   version='1.0.0',
@@ -16,17 +19,12 @@ setup(
   author_email='poehlmann@ucdavis.edu',
   url='https://github.com/mpoehlmann/velazquez_lab',
   packages=find_packages(exclude=('tutorials','scripts')),
-  # packages=['velazquez_lab',
-  #           'scintmc.scint',
-  #           'scintmc.utils',
-  #           'pyproto',
-  #           'pyproto.dselec',
-  #          ],
   package_dir={
     'velazquez_lab': 'velazquez_lab',
   },
   # scripts=['scripts/create_pol_curves.py',
   #         ],
   zip_safe=False,
+  install_requires=required,
 )
 
