4a8553d2ba3c5a84d37e0c92fca20ec6c1aefa62
Michael Poehlmann <poehlmann@ucdavis.edu>
2021-06-23 10:58:30 -0700

    Updated packaging implementation.

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ import numpy as np
 with open('README.md') as f:
   readme = f.read()
 
-with open('requirements.txt') as f:
-  required = f.read().splitlines()
-
 setup(
   name='velazquez_lab',
   version='1.0.0',
@@ -18,13 +15,7 @@ setup(
   author='Michael Poehlmann',
   author_email='poehlmann@ucdavis.edu',
   url='https://github.com/mpoehlmann/velazquez_lab',
-  packages=find_packages(exclude=('tutorials','scripts')),
-  # package_dir={
-  #   'velazquez_lab': 'velazquez_lab',
-  # },
-  # scripts=['scripts/create_pol_curves.py',
-  #         ],
+  packages=find_packages(exclude=['assets','data','docs','scripts']),
   zip_safe=False,
-#   install_requires=required,
 )
 
